ba7c54f9af39155f2417102d8fda4515c079c4f4
Dacadian <jammalradrex7@gmail.com>
2026-04-28 22:34:01 +0300

    Create Page.js

---
+++ app/Page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{fontSize:'3rem', textAlign:'center', color:'#f97316'}}>
+        Titan Tickets Homepage ✅
+      </h1>
+      <p style={{textAlign:'center', fontSize:'1.5rem'}}>
+        Next.js detected! Deploy success!
+      </p>
+    </main>
+  )
+}
